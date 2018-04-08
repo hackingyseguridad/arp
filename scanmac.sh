23770ac0d9f603fcd25343e2ed2b68bfbdf67558
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-04-08 21:08:19 +0200

    Create scanmac.sh

---
+++ scanmac.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Escanea y lista las IP = MAC Address"
+echo "Uso.: #sh scanmac.sh rango_IP"
+sudo nmap -sP $1 | awk '/Nmap scan report for/{printf $5;}/MAC Address:/{print " => "$3;}' | sort
