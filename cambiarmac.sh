fb654457ac45e3b9268e23db01394be0668b61b9
hackingyseguridad <antonio_taboada@hotmail.es>
2024-09-20 09:34:28 +0200

    Update cambiarmac.sh

--- cambiarmac.sh
+++ cambiarmac.sh
@@ -3,6 +3,10 @@
 # Otra opcion es con macchanger
 # apt-get install macchanger
 # macchanger -m 00:e0:4c:53:44:58 eth0
+# Otra Opcion es con IP
+# sudo ip link set eth0 down
+# ip link set eth0 address 00:e0:4c:53:44:58
+# sudo ip link set eth0 up
 #
 # hackingyseguridad.com ( 2024 )
 #
@@ -13,3 +17,9 @@ sudo ifconfig eth0 down
 
 # Actualizar IP con HDCP para la nueva MAC 
 # dhclient eth0
+
+ip link show eth0 | grep ether | awk '{print $2}'
+
+
+
+
